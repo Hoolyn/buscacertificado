c22a63204201ec8981c9a100ba73655e18f374ab
Moyses dos Santos <moysesspj@gmail.com>
2016-03-01 15:06:25 -0300

    improve home and config url

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://busca-certificado.herokuapp.com/',
+        url: 'http://buscacertificado.com.br/',
         fileStorage: false,
         mail: {
             transport: 'SMTP',
